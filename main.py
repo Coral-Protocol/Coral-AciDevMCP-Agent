3dd96aa6631ef469293e42860ab9731a9283c9c2
sd2879 <suman8deb@gmail.com>
2025-07-30 10:56:17 +0100

    updating agent file

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def create_agent(coral_tools, agent_tools):
         model_provider=os.getenv("MODEL_PROVIDER", "openai"),
         api_key=os.getenv("MODEL_API_KEY"),
         temperature=os.getenv("MODEL_TEMPERATURE", "0.1"),
-        max_tokens=os.getenv("MODEL_TOKEN", "8000"),
+        max_tokens=os.getenv("MODEL_MAX_TOKENS", "8000"),
         base_url=os.getenv("MODEL_BASE_URL") if os.getenv("MODEL_BASE_URL") else None
     )
     agent = create_tool_calling_agent(model, combined_tools, prompt)
